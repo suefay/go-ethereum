8c9753c89818c8a6969f20be38cb8d752a20052b
suefay <sufay30282@gmail.com>
2022-02-18 11:16:29 +0800

    add bootstrap nodes and static nodes to handler for peer filtering

--- eth/backend.go
+++ eth/backend.go
@@ -220,17 +220,17 @@ func New(stack *node.Node, config *ethconfig.Config) (*Ethereum, error) {
 		checkpoint = params.TrustedCheckpoints[genesisHash]
 	}
 	if eth.handler, err = newHandler(&handlerConfig{
-		Database:     chainDb,
-		Chain:        eth.blockchain,
-		TxPool:       eth.txPool,
-		Merger:       merger,
-		Network:      config.NetworkId,
-		Sync:         config.SyncMode,
-		BloomCache:   uint64(cacheLimit),
-		EventMux:     eth.eventMux,
-		Checkpoint:   checkpoint,
-		Whitelist:    config.Whitelist,
-		TrustedNodes: eth.p2pServer.TrustedNodes,
+		Database:       chainDb,
+		Chain:          eth.blockchain,
+		TxPool:         eth.txPool,
+		Merger:         merger,
+		Network:        config.NetworkId,
+		Sync:           config.SyncMode,
+		BloomCache:     uint64(cacheLimit),
+		EventMux:       eth.eventMux,
+		Checkpoint:     checkpoint,
+		Whitelist:      config.Whitelist,
+		WhitelistNodes: eth.whitelistNodes(),
 	}); err != nil {
 		return nil, err
 	}
@@ -583,4 +583,15 @@ func (s *Ethereum) Stop() error {
 	s.eventMux.Stop()
 
 	return nil
+}
+
+// whitelistNodes gets the whitelisted nodes which include bootstrap nodes, static nodes and trusted nodes.
+func (s *Ethereum) whitelistNodes() []*enode.Node {
+	nodes := make([]*enode.Node, 0)
+
+	nodes = append(nodes, s.p2pServer.BootstrapNodes...)
+	nodes = append(nodes, s.p2pServer.StaticNodes...)
+	nodes = append(nodes, s.p2pServer.TrustedNodes...)
+
+	return nodes
 }
